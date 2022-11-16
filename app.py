9ef79d294c50080653d41b28a8ea1b89f6590f09
rinriN <2210368@ecc.ac.jp>
2022-11-16 21:46:44 +0900

    :pencil2: add key and token into flask server

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('LZMLw49U/JiAxFJKgPUtIScTt0KlTMKfJwmTNL7Ec6OBAZeWcVYCHBm0m9nDlrKm7iDGmLl6gZikuXYG5U+OcfBKiHqIFK8LGk89Lqv46dbko6H7JnO/CrV7pFuP035wulgV6MYrHPzbQPfj5te36QdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('870b67c740b8012d0cc36ee0a116f925')
 
 
 @app.route("/callback", methods=['POST'])

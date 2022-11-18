4009ce9f0d67f16797728664b359c7a514a9aa36
rinriN <2210368@ecc.ac.jp>
2022-11-18 23:05:44 +0900

    :wrench: move import module from app.py to line_bot_api.py

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
 from flask import Flask, request, abort
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent, StickerSendMessage, ImageSendMessage,  LocationSendMessage, ImagemapSendMessage
-)
-from event.basic import about_us_event, location_event
-
+from line_bot_api import *
+from event.basic import *
 
 app = Flask(__name__)
-
-line_bot_api = LineBotApi('LZMLw49U/JiAxFJKgPUtIScTt0KlTMKfJwmTNL7Ec6OBAZeWcVYCHBm0m9nDlrKm7iDGmLl6gZikuXYG5U+OcfBKiHqIFK8LGk89Lqv46dbko6H7JnO/CrV7pFuP035wulgV6MYrHPzbQPfj5te36QdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('870b67c740b8012d0cc36ee0a116f925')
-
-
+ 
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value

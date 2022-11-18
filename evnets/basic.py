0abea420c3348ee69f40648040b940adc65b5162
rinriN <2210368@ecc.ac.jp>
2022-11-18 23:08:55 +0900

    :wrench: change the individual import method to uniform import of modules from the line bot api

--- evnets/basic.py
+++ evnets/basic.py
@@ -1,11 +1,4 @@
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent, StickerSendMessage, ImageSendMessage,  LocationSendMessage, ImagemapSendMessage
-)
-
-line_bot_api = LineBotApi('LZMLw49U/JiAxFJKgPUtIScTt0KlTMKfJwmTNL7Ec6OBAZeWcVYCHBm0m9nDlrKm7iDGmLl6gZikuXYG5U+OcfBKiHqIFK8LGk89Lqv46dbko6H7JnO/CrV7pFuP035wulgV6MYrHPzbQPfj5te36QdB04t89/1O/w1cDnyilFU=')
+from line_bot_api import *
 
 def about_us_event(event):
 

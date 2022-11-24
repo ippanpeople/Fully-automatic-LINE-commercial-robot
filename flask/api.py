76d75fb60efb8498f48c98f6f93ab0945e20e892
rinriN <2210368@ecc.ac.jp>
2022-11-24 17:29:49 +0900

    :pencil2: add PostbackEvent in api module to use on resevice message which post from user

--- flask/api.py
+++ flask/api.py
@@ -6,7 +6,8 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent, StickerSendMessage, ImageSendMessage,  LocationSendMessage, ImagemapSendMessage,
-    TemplateSendMessage, ImageCarouselTemplate, PostbackAction, ImageCarouselColumn
+    TemplateSendMessage, ImageCarouselTemplate, PostbackAction, ImageCarouselColumn,
+    PostbackEvent
 )
 line_bot_api = LineBotApi('LZMLw49U/JiAxFJKgPUtIScTt0KlTMKfJwmTNL7Ec6OBAZeWcVYCHBm0m9nDlrKm7iDGmLl6gZikuXYG5U+OcfBKiHqIFK8LGk89Lqv46dbko6H7JnO/CrV7pFuP035wulgV6MYrHPzbQPfj5te36QdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('870b67c740b8012d0cc36ee0a116f925')
